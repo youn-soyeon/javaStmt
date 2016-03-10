8f982ba59653ca07d616e56e513e1ead52ea902b
younsoyeon <sssoyeon92@naver.com>
2016-03-10 15:30:08 +0900

    NumGolf -bean -main

---
+++ javaStmt/src/whileloop/NumGolfBean.java
@@ -0,0 +1,63 @@
+package whileloop;
+
+public class NumGolfBean {
+	// 플레이어 : 게임 참여자가 맞히려고 입력한 값
+	// 컴 : 컴퓨터가 랜덤 생성한 값
+	// 카운트 : 플레이어가 입력한 횟수
+	// 카운트가 작을수록 승리
+	int player, com, count;
+	boolean flag; // 컴의 숫자와 일치하는지 체크하는 값
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(int player) {
+		this.player = player;
+
+		setFlag();
+		isFlag();
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom() {
+		// 1부터 10까지 랜덤 수 발생
+		this.com = (int) ((Math.random() * 10) + 1);
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount() {
+		this.count++;
+	}
+
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag() {
+		if (getCom() == getPlayer()) {
+			this.flag = true;
+		} else {
+			this.flag = false;
+		}
+		
+		System.out.println(toString());
+	}
+
+	@Override
+	public String toString() {
+		if (getCom() == getPlayer()) {
+			return "값이 일치합니다. \n"
+					+"결과 [플레이어 값=" + player + ", 컴퓨터 값=" + com + ", 시도한 횟수=" + count + ", 일치여부=" + flag + "]";
+		} else {
+			return "일치하지 않습니다.";
+		}
+	}
+
+}
---
+++ javaStmt/src/whileloop/NumGolfMain.java
@@ -0,0 +1,27 @@
+package whileloop;
+
+import java.util.Scanner;
+
+/**
+ * @file : NumGolfMain.java
+ * @author sssoyeon92@naver.com
+ * @date 2016. 3. 10.
+ * @story 컴퓨터가 랜덤생성한 숫자를 가장 빠른 횟수로 맞추는 게임
+ */
+
+public class NumGolfMain {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		NumGolfBean numGolfBean = new NumGolfBean();
+
+		System.out.println("컴퓨터 랜덤 수 발생");
+		numGolfBean.setCom();
+
+		while (numGolfBean.isFlag() == false) {
+			System.out.println("숫자를 입력하세요.");
+			numGolfBean.setPlayer(scanner.nextInt());
+			numGolfBean.setCount();
+		}
+
+	}
+}
