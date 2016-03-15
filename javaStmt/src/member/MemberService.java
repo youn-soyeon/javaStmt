3f8e17b93b97b71cb9e9b9b1f7dd9bd445aea663
younsoyeon <sssoyeon92@naver.com>
2016-03-15 18:18:47 +0900

    20160315 6:18 - Member 과제

---
+++ javaStmt/src/member/MemberService.java
@@ -0,0 +1,25 @@
+package member;
+
+public interface MemberService {
+	/**
+	 * 회원가입
+	 * */
+	public String join(MemberBean member);
+	/**
+	 * 회원로그인
+	 */
+	public String login(String id, String password);
+	/**
+	 * 내정보보기
+	 * */
+	public MemberBean checkInfo(String id, String password);
+	/**
+	 * 회원정보수정
+	 * */
+	public String update(String id, String password, String name, String addr, int birth);
+	/**
+	 * 회원탈퇴
+	 * */
+	public String remove(String id, String password);
+
+}
